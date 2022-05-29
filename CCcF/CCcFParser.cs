ff4695f11f4ad2d979a2d336fa42d3076660fe73
TheGBO <gabrielthejota@gmail.com>
2022-05-29 09:37:37 -0300

    Completed parser + File Input Example

--- CCcF/CCcFParser.cs
+++ CCcF/CCcFParser.cs
@@ -1,6 +1,39 @@
 using System;
-using System.Text;
+using System.Text.RegularExpressions;
 
-public class CCcFParser {
-    
+namespace CCcF
+{
+    public class CCcFParser {
+        private string content;
+
+        public CCcFParser(string content){
+            this.content = content;
+        }
+
+        public CCcFParser(){
+            this.content = "";
+        }
+
+        public void SetContentFromFile(string filePath){
+            this.content = File.ReadAllText(filePath);
+        }
+
+        public string GetString(string field){
+            Match match = Regex.Match(this.content, field+@"::(.*)");
+            string toParse = match.Groups[1].Value;
+            return toParse;
+        }
+
+        public int GetInt(string field){
+            return int.Parse(GetString(field));
+        }
+
+        public float GetFloat(string field){
+            return float.Parse(GetString(field));
+        }
+
+        public double GetDouble(string field){
+            return double.Parse(GetString(field));
+        }
+    }
 }
--- CCcF/Program.cs
+++ CCcF/Program.cs
@@ -1,6 +1,26 @@
 ﻿using System;
 using System.Text;
 
+using CCcF;
+
 public class Program {
-    
+    static void Main(string[] args)
+    {
+        string cccf = @"
+        Settings (text without double colon is a comment)
+
+        - Score::21
+
+        - Name::amonjos
+
+        - Game::Among us
+
+        ";
+
+        CCcFParser parser = new CCcFParser(cccf);
+        Console.WriteLine(parser.GetInt("Score"));
+        Console.WriteLine(parser.GetString("Name"));
+    }
 }
+
+
